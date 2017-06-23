ef16f2cdd8e551632d530ae5210a90c7a41d02c7
danleech-defra <danleech.defra@gmail.com>
2017-06-23 12:26:23 +0100

    Routes, pages and copy changes

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,30 +1,45 @@
-module.exports = [{
-  method: 'GET',
-  path: '/',
-  config: {
-    handler: function (request, reply) {
-      return reply.view('home/main')
-    }
-  }
-}, {
-  method: 'GET',
-  path: '/v1',
-  config: {
-    handler: function (request, reply) {
-      return reply.view('home/v1')
-    }
-  }
-}, {
-  method: 'GET',
-  path: '/v2',
-  config: {
-    handler: function (request, reply) {
-      const location = request.query.location
-
-      return reply.view('home/v2', {
-        pageTitle: `Live Flood Warnings for ${location} - GOV.UK`,
-        location: location
-      })
-    }
-  }
-}]
+module.exports = [
+	{
+		method: 'GET',
+		path: '/',
+		config: {
+			handler: function (request, reply) {
+				return reply.view('home/main')
+			}
+		}
+	},
+	{
+		method: 'GET',
+		path: '/v1',
+		config: {
+			handler: function (request, reply) {
+				return reply.view('home/v1')
+			}
+		}
+	},
+	{
+		method: 'GET',
+		path: '/v2',
+		config: {
+			handler: function (request, reply) {
+				const location = request.query.location
+				return reply.view('home/v2', {
+					pageTitle: `Live Flood Warnings for ${location} - GOV.UK`,
+					location: location
+				})
+			}
+		}
+	},
+	
+	// Location pages (Risk summary)
+	{
+		method: 'GET',
+		path: '/{location}',
+		config: {
+			handler: function (request, reply) {
+				const location = request.params.location.toLowerCase()
+				return reply.view('location/' + location)
+			}
+		}
+	}
+]
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,4 @@
 module.exports = [].concat(
   require('./home'),
-  require('./check-flood-risk'),
-  require('./live-warnings'),
   require('./public')
 )
