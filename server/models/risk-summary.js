b4cce850618a54f18b4513bb88c882d6bbcf0e04
danleech-defra <danleech-defra@gmail.com>
2017-06-29 13:41:21 +0100

    Model refactoring

--- server/models/risk-summary.js
+++ server/models/risk-summary.js
@@ -8,14 +8,25 @@ exports.getSummary = function(name, scenario) {
 		postcode, 
 		postcodes,  
 		location,
-		alertsByScenario = [],
+
+		allTargetAreas = [], // By scenario
+		intersectingTargetAreas = [], 
+		targetAreas = [],
 		alerts = [],
-		warningsByScenario = [],
 		warnings = [],
+
 		intersectingWarning = {},
 		intersectingAlert = {},
-		hasWarnings = false,
-		hasIntersect = false
+		intersectingTargetWarning = {},
+		intersectingTargetAlert = {},
+
+		hasTargetAreas = false, // Nearby
+		hasWarnings = false, // Nearby
+		hasAlerts = false, // Nearby
+		hasIntersectTarget = false,
+		hasIntersectWarning = false,
+		hasIntersectAlert = false,
+		hasIntersectAlertOrWarning = false
 
 	// Find in towns
 	town = data.town.find(
@@ -44,69 +55,70 @@ exports.getSummary = function(name, scenario) {
 		location = postcode.name + ', ' + town.name
 	}
 
-	// Get all warnings and alerts for scenario
-	warningsByScenario = data.targetArea.filter(x => x.scenario.indexOf(scenario) > -1 && x.severity === 2)
-	alertsByScenario = data.targetArea.filter(x => x.scenario.indexOf(scenario) > -1 && x.severity === 3)
+	// Get all target areas for scenario
+	allTargetAreas = data.targetArea.filter(x => x.scenario.indexOf(scenario) > -1)
 
-	// Add intersecting warning for postcode if in force
+	// Add intersections for postcode if applicable
 	if (postcode) {
-		intersectingWarning = warningsByScenario.find(x => x.id == postcode.intersectingWarning)
-		if (!intersectingWarning) {
-			intersectingAlert = alertsByScenario.find(x => x.id == postcode.intersectingAlert)
+		intersectingTargetWarning = allTargetAreas.find(x => x.id == postcode.intersectingWarning)
+		intersectingTargetAlert = allTargetAreas.find(x => x.id == postcode.intersectingAlert)
+		if (intersectingTargetWarning) {
+			if (intersectingTargetWarning.severity === 2) {
+				intersectingWarning = intersectingTargetWarning
+				hasIntersectAlertOrWarning = true
+				hasIntersectWarning = true
+			}
+		}
+		if (intersectingTargetAlert) {
+			if (intersectingTargetAlert.severity === 3) {
+				intersectingAlert = intersectingTargetAlert
+				hasIntersectAlertOrWarning = true
+				hasIntersectAlert = true
+			}	
 		}
 	}
 
-	// Add nearby warnings for postcode
+	// Add nearby targets, warnings and alerts for postcodes
 	if (postcodes) {
-		for (var key in warningsByScenario) {
-			var warning = warningsByScenario[key]
+		for (var key in allTargetAreas) {
+			var targetArea = allTargetAreas[key]
 			for (var key in postcodes) {
-				if (postcodes[key].targetAreas.indexOf(warning.id) > -1) {
-					if (warnings.indexOf(warning) == -1 && warning != intersectingWarning) {
-						warnings.push(warning)
+				if (postcodes[key].targetAreas.indexOf(targetArea.id) > -1) {
+					// Add targetArea
+					targetAreas.push(targetArea)
+					hasTargetAreas = true
+					// Add nearby warning, excluding intersecting warning
+					if (warnings.indexOf(targetArea) == -1 && targetArea != intersectingTargetWarning && targetArea.severity == 2) {
+						warnings.push(targetArea)
+						hasWarnings = true
+					}
+					// Add nearby alert, excluding one scenario 
+					if (alerts.indexOf(targetArea) == -1 && targetArea.severity == 3 && !(!hasIntersectWarning && hasIntersectAlert)) {
+						alerts.push(targetArea)
+						hasAlerts = true
 					}
 				}
 			}
 		}
-	}
-
-	// Add nearby alerts for postcode
-	if (postcodes) {
-		for (var key in alertsByScenario) {
-			var alert = alertsByScenario[key]
-			for (var key in postcodes) {
-				if (postcodes[key].targetAreas.indexOf(alert.id) > -1) {
-					if (alerts.indexOf(alert) == -1 && alert != intersectingAlert) {
-						alerts.push(alert)
-					}
-				}
-			}
-		}
-	}
-
-	//
-	// Set booleans
-	//
-
-	// Set hasWarnings property
-	if (warnings.length || alerts.length || JSON.stringify(intersectingWarning) != '{}' || JSON.stringify(intersectingAlert) != '{}') {
-		hasWarnings = true
-	}
-
-	// Set hasWarnings property
-	if (JSON.stringify(intersectingWarning) != '{}' || JSON.stringify(intersectingAlert) != '{}') {
-		hasIntersect = true
 	}
 
 	// Build model and return
 	return {
 		location,
-		warnings,
-		alerts,
+		warnings, // Nearby warnings
+		alerts, // Nearby alerts
+		targetAreas, // Nearby target areas
+		intersectingTargetWarning,
 		intersectingWarning,
+		intersectingTargetAlert,
 		intersectingAlert,
+		hasTargetAreas,
 		hasWarnings,
-		hasIntersect
+		hasAlerts,
+		hasIntersectTarget,
+		hasIntersectWarning,
+		hasIntersectAlert,
+		hasIntersectAlertOrWarning
 	}
 
 }
