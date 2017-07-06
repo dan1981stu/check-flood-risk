c8690952ffefa539c69a6c7e85aeb9c9cea8a1ee
danleech-defra <danleech-defra@gmail.com>
2017-07-06 13:12:04 +0100

    Logic fix

--- server/models/home.js
+++ server/models/home.js
@@ -10,12 +10,11 @@ exports.getSummary = function(name, scenario) {
 		location,
 		lonLat, // Of location
 
-		intersectingTargetAreas = [], 
 		intersectingTarget = {},
 		warningsSevere = [], // Nearby
 		warnings = [], // Nearby
 		alerts = [], // Nearby
-		targetAreas = [], // Nearby
+		targetAreaAll = [], // Nearby
 		levels = [], // Levels for impact area
 		impacts = [], // Current impacts in this area
 		levelState = 'normal', // Level state summary
@@ -80,12 +79,13 @@ exports.getSummary = function(name, scenario) {
 		var intersectingTargetWarning = data.targetArea.find(x => x.id == postcode.intersectingWarning)
 		var intersectingTargetAlert = data.targetArea.find(x => x.id == postcode.intersectingAlert)
 		if (intersectingTargetAlert) {
-			// Intersets with an target alert area
+			// Intersets with a target alert area
 			hasIntersectTarget = true
 			if (intersectingTargetAlert.scenario.find(x => x.id == scenario && x.severity == 3)) {
 				// Intersects with a current alert
 				intersectingTarget = intersectingTargetAlert
 				targetAreaStates.push({ "id" : intersectingTarget.id, "state" : 3 })
+				targetAreaAll.push(intersectingTarget)
 				hasIntersectAlert = true
 			}	
 		}
@@ -96,11 +96,13 @@ exports.getSummary = function(name, scenario) {
 				// Intersects with a current severe warning
 				intersectingTarget = intersectingTargetWarning
 				targetAreaStates.push({ "id" : intersectingTarget.id, "state" : 1 })
+				targetAreaAll.push(intersectingTarget)
 				hasIntersectWarningSevere = true
 			} else if (intersectingTargetWarning.scenario.find(x => x.id == scenario && x.severity == 2)) {
 				// Intersects with a current warning
 				intersectingTarget = intersectingTargetWarning
 				targetAreaStates.push({ "id" : intersectingTarget.id, "state" : 2 })
+				targetAreaAll.push(intersectingTargeta)
 				hasIntersectWarning = true
 			}
 		}
@@ -114,7 +116,7 @@ exports.getSummary = function(name, scenario) {
 				if (postcodes[key].targetAreas.indexOf(targetArea.id) > -1) {
 					// Add nearby severe warning, excluding intersecting severe warning
 					if (warningsSevere.indexOf(targetArea) == -1 && targetArea != intersectingTarget && targetArea.scenario.find(x => x.id == scenario && x.severity == 1)) {
-						targetAreas.push(targetArea)
+						targetAreaAll.push(targetArea)
 						warningsSevere.push(targetArea)
 						targetAreaStates.push({ "id" : targetArea.id, "state" : 1 })
 						hasWarningSevere = true
@@ -122,14 +124,14 @@ exports.getSummary = function(name, scenario) {
 					// Add nearby warning, excluding intersecting warning
 					if (warnings.indexOf(targetArea) == -1 && targetArea != intersectingTarget && targetArea.scenario.find(x => x.id == scenario && x.severity == 2)) {
 						warnings.push(targetArea)
-						targetAreas.push(targetArea)
+						targetAreaAll.push(targetArea)
 						targetAreaStates.push({ "id" : targetArea.id, "state" : 2 })
 						hasWarning = true
 					}
 					// Add nearby alert, excluding 1/4 scenario 
 					if (alerts.indexOf(targetArea) == -1 && targetArea.scenario.find(x => x.id == scenario && x.severity == 3) && !(!hasIntersectWarning && !hasIntersectWarningSevere && hasIntersectAlert)) {
 						alerts.push(targetArea)
-						targetAreas.push(targetArea)
+						targetAreaAll.push(targetArea)
 						targetAreaStates.push({ "id" : targetArea.id, "state" : 3 })
 						hasAlert = true
 					}
@@ -141,8 +143,8 @@ exports.getSummary = function(name, scenario) {
 	// Add river levels and set general level state (Normal, above or below)
 	for (var key in data.level) {
 		var level = data.level[key]
-		for (var key in targetAreas) {
-			var targetArea = targetAreas[key]
+		for (var key in targetAreaAll) {
+			var targetArea = targetAreaAll[key]
 			if (targetArea.levels.indexOf(level.id) > -1 && levels.indexOf(level) == -1) {
 				// Add level
 				levels.push(level)
@@ -236,7 +238,7 @@ exports.getSummary = function(name, scenario) {
 		warningsSevere, // List of nearby severe warnings
 		warnings, // List of nearby warnings
 		alerts, // List of nearby alerts
-		targetAreas, // List of nearby target areas
+		targetAreaAll, // List of nearby target areas
 		intersectingTarget, // The intersecting target area
 
 		levelState, // Above, normal or below
