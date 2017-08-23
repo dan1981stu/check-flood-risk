9710ffb03480efc4330ecbb76eafc3b8370b5945
danleech-defra <danleech.defra@gmail.com>
2017-08-23 14:24:21 +0100

    Report flood start page

--- server/routes/gov-uk.js
+++ server/routes/gov-uk.js
@@ -9,8 +9,8 @@ module.exports = [
 				const scenario = request.query.s ? request.query.s : 'a'
 				return reply.view('gov-uk/check-flood-risk', {
 					'model' : { 'scenario' : scenario },
-					'pageTitle' : '',
-					'serviceName' : ''
+					'pageTitle' : 'Check flood risk - GOV.UK',
+					'serviceName' : 'Check flood risk'
 				})
 			}
 		}
@@ -43,6 +43,22 @@ module.exports = [
 					'model' : { 'scenario' : scenario },
 					'pageTitle' : 'Get flood warnings - GOV.UK',
 					'serviceName' : 'Get flood warnings'
+				})
+			}
+		}
+	},
+
+	// Report a flood - Service start page (GOV.UK)
+	{
+		method: 'GET',
+		path: '/report-a-flood',
+		config: {
+			handler: function (request, reply) {
+				const scenario = request.query.s ? request.query.s : 'a'
+				return reply.view('gov-uk/report-a-flood', {
+					'model' : { 'scenario' : scenario },
+					'pageTitle' : 'Report a flood - GOV.UK',
+					'serviceName' : 'Report a flood'
 				})
 			}
 		}
