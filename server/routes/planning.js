28b0237990881b9fde5f1bca17a535aeeb1067ae
danleech-defra <danleech-defra@gmail.com>
2017-11-09 10:51:49 +0000

    GDS Show hide assets added

--- server/routes/planning.js
+++ server/routes/planning.js
@@ -10,5 +10,17 @@ module.exports = [
 				})
 			}
 		}
+	},
+	{
+		method: 'GET',
+		path: '/flood-risk-assessment/identify-boundary',
+		config: {
+			handler: function (request, reply) {
+				return reply.view('planning/site', {
+					'serviceName' : 'Check flood zone',
+					'pageTitle' : 'Identify boundary - Check flood zone - GOV.UK'
+				})
+			}
+		}
 	}
 ]
