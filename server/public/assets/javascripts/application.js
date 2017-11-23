602c2fafcf9ec0d189e1809be9609ba2b21a4cfd
danleech-defra <danleech.defra@gmail.com>
2017-11-23 13:44:09 +0000

    Text amends

--- server/public/assets/javascripts/application.js
+++ server/public/assets/javascripts/application.js
@@ -5,13 +5,11 @@ var application = {};
 
   var navBack = document.getElementsByClassName('nav-back')
   if (navBack.length) {
-    console.log(navBack.length)
     navBack[0].style.display = 'block'
     navBack[0].removeAttribute('hidden')
-    e.preventDefault()
-    navBack[0].addEventListener('click', {
-      console.log('click')
-      //window.history.back()
+    navBack[0].addEventListener('click', function(e) {
+      e.preventDefault()
+      window.history.back()
     })
   }
 
