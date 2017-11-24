20a87b6d21445e3754be4094da5513e2dd3522b5
danleech-defra <danleech.defra@gmail.com>
2017-11-24 14:57:48 +0000

    Structure changes

--- server/models/property.js
+++ server/models/property.js
@@ -1,43 +1,127 @@
+const utilities = require('../utilities/utilities.js')
 var data = require('../data/data.json')
 
 // Get a location by its name
-exports.getProperty = function(premises, postcode, scenario) {
+exports.getProperty = function(premises, postcode, scenario, error) {
 
-	var property = [], postcodes = [], country
+	error = error || false
+	premises = premises || ''
+	postcode = postcode || ''
+	scenario = scenario || 'a'
 
-	// Find postcode
-	p = data.postcode.find( y => y.path == postcode)
-	pId = p != null ? p.id : -1
+	var model = {}, property = []
 
-	// Filter firstline
-	firstLine = data.firstLine.filter(function(x){
-		// If premises containes a name, flat or apartment
-		if (isNaN(x.premises)) {
-			var premisesStreet = x.premises.concat(', ' + x.street).toLowerCase()
-			return premisesStreet.includes(premises.toLowerCase()) ? true : false
-		} 
-		// If premises is a number
-		else {
-			return x.premises.toLowerCase() == premises.toLowerCase() ? true : false
-		}
-	})
+	// Form values to model
+	model['premises'] = premises
+	model['postcode'] = postcode
+	model['scenario'] = scenario
+
+	// Try to set postcode to object
+	postcode = data.postcode.find( x => x.path == postcode.replace(/ /g,'-').toLowerCase() )
 	
-	// If firstLine(s) found build address list
-	firstLine.forEach(function (item) {
-		// Add comma to non number premises
-		premises = isNaN(item.premises) ? item.premises + ', ' : item.premises
-		// Get postcode for address
-		postcode = data.postcode.find( x => x.id == item.postcodeId)
-		// Get town for postcode
-		town = data.town.find( x => x.id == postcode.townId)
-		// Concatenate the address
-		address = premises + ' ' + item.street + ', ' + town.name + ', ' + postcode.name
-		// Create the path
-		path = address.replace(/,\s+/g,'/').replace(/\s+/g, '-').toLowerCase()
-		// Add item to list
-		property.push({ 'address' : address, 'path' : path, 'country' : postcode.country })
-	})
+	// Tidy inputs
+	premises = premises.toLowerCase()
+	scenario = scenario.toLowerCase()
 
-	return property
+	// Set country properties if postcode exists
+	if (typeof postcode === 'object') {
+		var countryCode , countryName
+		switch(postcode.country) {
+			case 's':
+				countryCode = 's'
+				countryName = 'Scotland'
+				break
+			case 'w':
+				countryCode = 'w'
+				countryName = 'Wales'
+				break
+			case 'ni':
+				countryCode = 'ni'
+				countryName = 'Northern Ireland'
+				break
+			default:
+				countryCode = 'e'
+				countryName = 'England'
+		}
+		model['countryCode'] = countryCode
+		model['countryName'] = countryName
+
+		// Set is England boolean
+		if (countryCode == 'e') {
+			model['isEngland'] = true
+		} else {
+			model['isEngland'] = false
+		}
+
+		// Filter firstline if premises and postcode given
+		if (premises != '') {
+			firstLine = data.firstLine.filter(function(x){
+				// If premises containes a name, flat or apartment
+				if (isNaN(x.premises)) {
+					var premisesStreet = x.premises.concat(', ' + x.street).toLowerCase()
+					return premisesStreet.includes(premises.toLowerCase()) ? true : false
+				} 
+				// If premises is a number
+				else {
+					return x.premises.toLowerCase() == premises.toLowerCase() ? true : false
+				}
+			})
+			// If firstLine(s) found build property/address list
+			if (firstLine.length) {
+				firstLine.forEach(function (item) {
+					// Add comma to non number premises
+					itemPremises = isNaN(item.premises) ? item.premises + ', ' : item.premises
+					// Get street
+					itemStreet = item.street
+					// Get postcode for address
+					itemPostcode = postcode.name
+					// Get town for postcode
+					itemTown = data.town.find( x => x.id == postcode.townId).name
+					// Concatenate the address
+					address = itemPremises + ' ' + itemStreet + ', ' + itemTown + ', ' + itemPostcode
+					// Create the path
+					path = address.replace(/,\s+/g,'/').replace(/\s+/g, '-').toLowerCase()
+					// Add item to list
+					property.push({ 'address' : address, 'path' : path })
+				})
+				model['property'] = property
+			}
+		}
+	}
+
+	// Set has existing address boolean
+	if (model['property']) {
+		model['hasExistingAddress'] = true
+	} else {
+		model['hasExistingAddress'] = false
+	}
+	
+	// Set is single result boolean
+	if (model['property']) {
+		if (model.property.length == 1) {
+			model['isSingle'] = true
+		} 
+	} else {
+		model['isSingle'] = false
+	}
+
+	// If no existing address add error details
+	if (!model.hasExistingAddress) {
+		model['isError'] = true
+		model['errors'] = { 'address' : { 'type' : 'any.empty', 'message' : '' } }
+	}
+
+	// If error add error details
+	if (error) {
+		var errors
+		if(error && error.data) {
+			errors = utilities.extractValidationErrors(error) // the error field + message
+		}
+		model['isError'] = true
+		model['errors'] = errors
+	}
+
+	// Return model
+	return model
 
 }
--- server/routes/property.js
+++ server/routes/property.js
@@ -1,4 +1,3 @@
-const utilities = require('../utilities/utilities.js')
 const modelData = require('../models/property')
 var Joi = require('joi')
 
@@ -8,11 +7,14 @@ module.exports = [
 		path: '/find-address',
 		config: {
 			handler: function (request, reply) {
+
 				const scenario = request.query.s ? request.query.s : 'a'
+
 				return reply.view('property/find-address', {
 					'pageTitle' : 'Find address - Property flood risk - GOV.UK',
 					'model' : { 'scenario' : scenario }
 				})
+
 			}
 		}
 	},
@@ -23,47 +25,33 @@ module.exports = [
 			handler: function (request, reply) {
 
 				const scenario = request.payload.scenario ? request.payload.scenario : 'a'
-				var premises = request.payload.premises.toLowerCase()
-				var postcode = request.payload.postcode.replace(/ /g,'-').toLowerCase()
-				var property = modelData.getProperty(premises, postcode, scenario)
+				var premises = request.payload.premises
+				var postcode = request.payload.postcode
+				var model = modelData.getProperty(premises, postcode, scenario)
 				
-				// If we have a valid address(s)
-				if (property.length) {
+				// If we have an one or more existing addresses
+				if (model.hasExistingAddress) {
 
-					var country = { 'code' : property[0].country, 'name' : '' }
-					switch(country.code) {
-						case 's':
-							country.name = 'Scotland'
-							break
-						case 'w':
-							country.name = 'Wales'
-							break
-						case 'w':
-							country.name = 'Northern Ireland'
-							break
-						default:
-							country.name = 'England'
+					// If postcode is England
+					if (model.isEngland) {
+						return reply.redirect('/select-address?premises=' + model.premises + '&postcode=' + model.postcode + '&s='+ scenario)
 					}
 
-					// Address(s) are in England
-					if (country.code == 'e') {
-						return reply.redirect('/select-address?premises=' + premises + '&postcode=' + postcode + '&s='+ scenario)
-					}
-
-					// Address(s) are outside England
+					// Postcode is in Scotland, Wales or Northern Ireland
 					else {
 						return reply.view('property/alternate-service', {
 							'pageTitle' : 'Error: Find address - Property flood risk - GOV.UK',
-							'model' : { 'errors' : { 'country' : country } }
+							'model' : model
 						})
 					}
 
 				}
-				// We don't have a valid address
+
+				// We don't have an existing address
 				else {
 					return reply.view('property/find-address', {
 						'pageTitle' : 'Error: Find address - Property flood risk - GOV.UK',
-						'model' : { 'errors': { 'address' : { 'type' : 'any.empty', 'message' : '' }}, 'values' : { 'premises' : request.payload.premises, 'postcode' : request.payload.postcode, 'scenario' : scenario }, 'scenario' : scenario }
+						'model' : model
 					}) // .code(error ? 400 : 200) // HTTP status code depending on error
 				}
 
@@ -77,16 +65,17 @@ module.exports = [
 					postcode: Joi.string().required()
 				},
 				failAction: function (request, reply, source, error) {
-					var errors, values
+
 					const scenario = request.payload.scenario ? request.payload.scenario : 'a'
-					if(error && error.data) {
-						errors = utilities.extractValidationErrors(error) // the error field + message
-						values = utilities.getFormInputValues(error) // avoid wiping form data
-					}
+					var premises = request.payload.premises
+					var postcode = request.payload.postcode
+					var model = modelData.getProperty(premises, postcode, scenario, error)
+					
 					return reply.view('property/find-address', {
 						'pageTitle' : 'Error: Find address - Property flood risk - GOV.UK',
-						'model' : { 'errors'  : errors, 'values' : values, 'scenario' : scenario }
+						'model' : model
 					}) // .code(error ? 400 : 200) // HTTP status code depending on error
+
 				}
 			}
 		}
@@ -96,21 +85,22 @@ module.exports = [
 		path: '/select-address',
 		config: {
 			handler: function (request, reply) {
+
 				const scenario = request.query.s ? request.query.s : 'a'
 				const premises = request.query.premises ? request.query.premises : 'error'
-				const postcode = request.query.postcode ? request.query.postcode.replace(/\-/g,' ').toUpperCase() : 'error'
+				const postcode = request.query.postcode ? request.query.postcode : 'error'
 
 				if (premises === 'error' || postcode === 'error') {
 					return reply.view('404').code(404)
 				}
 				
-				var property = modelData.getProperty(premises, postcode, scenario)
-				var isSingle = property.length > 1 ? false : true
+				var model = modelData.getProperty(premises, postcode, scenario)
 				
 				return reply.view('property/select-address', {
 					'pageTitle' : 'Select address - Property flood risk - GOV.UK',
-					'model' : { 'property' : property, 'isSingle' : isSingle, 'premises' : premises, 'postcode' : postcode, 'scenario' : scenario }
+					'model' : model
 				})
+
 			}
 		}
 	},
@@ -119,9 +109,12 @@ module.exports = [
 		path: '/select-address',
 		config: {
 			handler: function (request, reply) {
+
 				const scenario = request.payload.scenario ? request.payload.scenario : 'a'
-				const property = request.payload.property ? request.payload.property : ''
+				const property = request.payload.property
+
 				return reply.redirect('/' + property + '?s='+ scenario)
+
 			},
 			validate: {
 				options: {
@@ -131,21 +124,15 @@ module.exports = [
 					property: Joi.string().required()
 				},
 				failAction: function (request, reply, source, error) {
+
 					const scenario = request.payload.scenario ? request.payload.scenario : 'a'
-					const premises = request.payload.premises ? request.payload.premises : 'error'
-					const postcode = request.payload.postcode ? request.payload.postcode.replace(/\-/g,' ').toUpperCase() : 'error'
+					var premises = request.payload.premises
+					var postcode = request.payload.postcode
+					var model = modelData.getProperty(premises, postcode, scenario, error)
 
-					var property = modelData.getProperty(premises, postcode)
-					var isSingle = property.length > 1 ? false : true
-
-					var errors, values
-					if(error && error.data) {
-						errors = utilities.extractValidationErrors(error) // the error field + message
-						values = utilities.getFormInputValues(error) // avoid wiping form data
-					}
 					return reply.view('property/select-address', {
 						'pageTitle' : 'Error: Select address - Property flood risk - GOV.UK',
-						'model' : { 'property' : property, 'errors'  : errors, 'values' : values, 'isSingle' : isSingle, 'premises' : premises, 'postcode' : postcode, 'scenario' : scenario }
+						'model' : model
 					}) // .code(error ? 400 : 200) // HTTP status code depending on error
 				}
 			}
